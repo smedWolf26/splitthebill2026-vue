327fe3aedc22669ccddefbef8a61b8a3a6cd1914
smedWolf26 <smedina.lambda@gmail.com>
2026-02-08 16:06:29 -0600

    added basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,45 @@
 <script setup>
-
+    import Header from './components/Header.vue'
 </script>
 
 <template>
-<h1> hello there</h1>
+    <Header/>
+    <main class="card">
+        <form id="personForm" class="rowForm">
+            <input id="personInput" type="text" placeholder="Add persons name"/>
+            <button>Add Person</button>
+        </form>
+
+        <form id="expenseForm" class="rowForm">
+             <input id="descInput" type="text" placeholder="Expense Description" />
+             <input id="amountInput" type="number" placeholder="amount" />
+             <select id="paidBySelect"></select>
+             <button>Add Expense</button>
+        </form>
+        <section>
+            <h3>People</h3>
+            <ul id="peopleList" class="list"></ul>
+        </section>
+
+        <section>
+            <h3>Expenses</h3>
+                  <ul id="expenseList" class="list"></ul>
+         
+              
+        </section>
+            <h3>Total</h3>
+            <p>
+                Total Spent: <strong id="totalSpent" >$0.00</strong><br/>
+                Split Per Person <strong id="splitAmount" >$0.00</strong>
+            </p>
+        <section>
+
+        </section>
+        <section class="summary">
+            <h3>Summary</h3>
+                <ul id="summaryList" class="list"></ul>
+        </section>
+    </main>
 </template>
 
 <style scoped>
