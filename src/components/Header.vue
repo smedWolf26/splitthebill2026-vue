fc99afe3236c57a6465fa0a26d2bb6ae1e9f44f7
smedWolf26 <smedina.lambda@gmail.com>
2026-02-08 16:03:09 -0600

    added header

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+    <template>
+        <header class="top">
+            <h1>Split The Bill</h1>
+            <p>Split expenses and see who owes what.</p>
+        </header>
+</template>
+    
+
+<style scoped>
+.top {
+    text-align:center;
+    margin-bottom: 16px;
+}
+
+.top h1 {
+    margin: 0;
+}
+
+.top p {
+    margin: 4px 0 0 ;
+    opacity: 0.7;
+}
+
+</style>
